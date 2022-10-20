013c35429f2386439a0d17f43c52c5576a8fce9a
marsyadewanti <marsyadewanti67@gmail.com>
2022-10-20 15:51:11 +0700

    first commit

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Marsya</li>
+        <li>Nama Belakang : Dewanti Khairunissa</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Kedungleri RT 001/RW 003 Saradan,Baturetno,Wonogiri</li>
+    </ul>
+ 
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Cirebon</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Badminton</li>
+        <li>Jooging</li>
+    </ul>
+</section>
